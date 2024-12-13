d2a7b7256179d031a92479a8a751eea0af4d6b62
zphrs <z@zephiris.dev>
2024-12-12 16:01:50 -0800

    fixed up template

--- days/template/src/main.rs
+++ days/template/src/main.rs
@@ -1,23 +1,26 @@
 use std::fmt::Display;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
+use std::iter::Map;
 
-pub fn part1_solution(lines: Lines<BufReader<File>>) -> impl Display {
-    todo!();
+pub fn part1_solution(
+    lines: Map<Lines<BufReader<File>>, impl FnMut(Result<String, io::Error>) -> String>,
+) -> impl Display {
     "todo"
 }
 
-pub fn part2_solution(lines: Lines<BufReader<File>>) -> impl Display {
-    todo!();
+pub fn part2_solution(
+    lines: Map<Lines<BufReader<File>>, impl FnMut(Result<String, io::Error>) -> String>,
+) -> impl Display {
     "todo"
 }
 
 fn main() {
     let file = File::open("input").unwrap();
-    let lines = io::BufReader::new(file).lines();
+    let lines = io::BufReader::new(file).lines().map(|l| l.unwrap());
     println!("Part 1: {}", part1_solution(lines));
     let file = File::open("input").unwrap();
-    let lines = io::BufReader::new(file).lines();
+    let lines = io::BufReader::new(file).lines().map(|l| l.unwrap());
     println!("Part 2: {}", part2_solution(lines));
 }
 
